3f05a8f5ad464b804b49f3acef719b5801534c1c
Kazimierz Wojciechowski <kazimierz.9200@gmail.com>
2018-10-10 23:00:39 +0200

    Add project files.

---
+++ SubgraphIsomorphismBenchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SubgraphIsomorphismBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
