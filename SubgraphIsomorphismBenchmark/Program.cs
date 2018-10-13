5b5745d61e0133f4b2bad21a21a646de16bc7e57
Kazimierz Wojciechowski <kazimierz.9200@gmail.com>
2018-10-13 12:31:59 +0200

    Changed benchmarks

--- SubgraphIsomorphismBenchmark/Program.cs
+++ SubgraphIsomorphismBenchmark/Program.cs
@@ -1,36 +1,51 @@
 ﻿using GraphDataStructure;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 
 namespace SubgraphIsomorphismBenchmark
 {
     class Program
     {
+        private const string path = @"benchmark.csv";
         static void Main(string[] args)
         {
-            printBenchmark(1, 0.9);
-            printBenchmark(16, 0.9);
-            printBenchmark(17, 0.9);
-            printBenchmark(18, 0.9);
-            printBenchmark(19, 0.9);
+            File.WriteAllText(path, string.Empty);
+            printBenchmark(15, 0.01m);
         }
-        private const int iterations = 1;
-        private static void printBenchmark(int n, double density)
+        private const int oddIterations = 0;
+        private static void printBenchmark(int n, decimal density)
         {
-            var time = TimeSpan.Zero;
-            for (int i = 0; i < iterations; i++)
+            var results = new List<TimeSpan>();
+            for (int i = 1; i <= oddIterations * 2 + 1; i++)
             {
-                time += BenchmarkIsomorphism(n, density, i);
+                results.Add(BenchmarkIsomorphism(n, (double)density, i));
             }
-            Console.WriteLine($"{n}: Elapsed: {time.TotalMilliseconds / iterations}ms");
-            printBenchmark(n+1, density);
+            results.Sort();
+            var medianTime = results[results.Count / 2];
+            Console.WriteLine($"{n}, {density}: Elapsed: {medianTime.TotalMilliseconds}ms");
+            using (var sw = File.AppendText(path))
+            {
+                sw.WriteLine($"{n},{density},{medianTime.TotalMilliseconds}");
+            }
+            //if (density < 0.6m)
+            //{
+            //    printBenchmark(n, density + 0.05m);
+            //}
+            //else
+            //{
+            //    printBenchmark(n + 1, 0.1m);
+            //}
+            printBenchmark(n, density + 0.01m);
         }
 
         private static TimeSpan BenchmarkIsomorphism(int n, double density, int seed)
         {
             var sw = new Stopwatch();
             var g = GraphFactory.GenerateRandom(n, density, seed);
-            var h = GraphFactory.GeneratePermuted(g, 0);
+            var h = GraphFactory.GenerateRandom(n, density, -seed);
+            //var h = GraphFactory.GeneratePermuted(g, 0);
 
             // run the algorithm
             var solver = new SubgraphIsomorphismExactAlgorithm.AlphaSubgraphIsomorphismExtractor<int>();
